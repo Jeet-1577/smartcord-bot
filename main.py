324c1cff440aad020ea8f27b32b1071dd6160332
Jeet-1577 <mr.jac5333@gmail.com>
2025-07-21 10:52:19 +0530

    .env added with discord token and calling that token into the main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# This example requires the 'message_content' intent.
+import os
+from dotenv import load_dotenv
 import discord
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 client = MyClient(intents=intents)
-client.run('token')
+client.run(TOKEN)
